21579e9640498f5632c49d9e331efa1e1ad4cbd3
moingshoing <yapsuperman@gmail.com>
2025-01-22 20:23:32 +0800

    customise

--- src/main/java/Aris.java
+++ src/main/java/Aris.java
@@ -1,11 +1,20 @@
-public class Arona {
+import java.util.Scanner;
+
+public class Aris {
     public static void main(String[] args) {
-        String logo =
-                  " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String hline = "____________________________________________________________\n";
+        String greet = "Panpakapan! I'm Aris! \nWhat can I do for you?\n";
+        String exit = "Bye :(\n";
+        System.out.println(hline + greet + hline); // greet
+
+        Scanner userInput =  new Scanner(System.in); // read user input
+        String input =  userInput.nextLine();
+
+        while (!input.equals("bye")) { // exit the program when user types bye
+            System.out.println(input);
+            input = userInput.nextLine();
+        }
+
+        System.out.println(exit + hline); // exit
     }
 }
