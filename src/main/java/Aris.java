82ea4551c5a1d8a82e43c391e0e0afb4b267e37c
moingshoing <yapsuperman@gmail.com>
2025-01-22 20:22:19 +0800

    lvl 1

--- src/main/java/Aris.java
+++ src/main/java/Aris.java
@@ -1,6 +1,7 @@
-public class Duke {
+public class Arona {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        String logo =
+                  " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
