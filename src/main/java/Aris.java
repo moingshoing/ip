359b8f6a57451515638a4caea7c8eaef058368ad
moingshoing <yapsuperman@gmail.com>
2025-01-22 20:24:54 +0800

    a

--- src/main/java/Aris.java
+++ src/main/java/Aris.java
@@ -4,14 +4,14 @@ public class Aris {
     public static void main(String[] args) {
         String hline = "____________________________________________________________\n";
         String greet = "Panpakapan! I'm Aris! \nWhat can I do for you?\n";
-        String exit = "Bye :(\n";
+        String exit = "NOOOOOO COME BACK PLEASE :(\n";
         System.out.println(hline + greet + hline); // greet
 
         Scanner userInput =  new Scanner(System.in); // read user input
         String input =  userInput.nextLine();
 
         while (!input.equals("bye")) { // exit the program when user types bye
-            System.out.println(input);
+            System.out.println(input + hline);
             input = userInput.nextLine();
         }
 
