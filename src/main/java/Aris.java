eae66c3fed6c291da1f1f6531c23f0c24cd73a95
moingshoing <yapsuperman@gmail.com>
2025-01-22 21:13:10 +0800

    lvl 2

--- src/main/java/Aris.java
+++ src/main/java/Aris.java
@@ -5,11 +5,26 @@ public class Aris {
         UI Ui = new UI(); // UI for format messages
         Ui.greet(); // greet
 
+        String[] list = new String[100];
+        int count = 0;
+
         Scanner userInput =  new Scanner(System.in); // read user input
         String input =  userInput.nextLine();
 
         while (!input.equals("bye")) { // exit the program when user types bye
-            Ui.format(input);
+            if (input.equals("list")) {
+                String s = "";
+                for (int i = 1; i < count + 1; i++) {
+                    s += i + ". " + list[i - 1] + "\n";
+                }
+                Ui.format(s);
+            } else {
+                Ui.format("added: " + input);
+
+                list[count] = input;
+                count++;
+            }
+
             input = userInput.nextLine();
         }
 
