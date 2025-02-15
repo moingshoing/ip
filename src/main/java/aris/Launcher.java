484297e308cf176c8d1a1864e6a3c96aae89ab94
moingshoing <yapsuperman@gmail.com>
2025-02-15 16:30:20 +0800

    Remove JavaDoc comment

--- src/main/java/aris/Launcher.java
+++ src/main/java/aris/Launcher.java
@@ -6,10 +6,6 @@ import javafx.application.Application;
  * The Launcher class serves as the entry point to launch the application.
  */
 public class Launcher {
-    /**
-     * The main method that launches the JavaFX application.
-     * @param args The command-line arguments.
-     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
